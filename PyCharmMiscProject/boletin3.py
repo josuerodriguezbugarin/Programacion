5ff66bd40a9eae24de59e97fd1799800f6b444f2
josue <josuerodriguezbuarin@danielcastelao.org>
2025-10-10 10:31:24 +0200

    Boletin 3

---
+++ PyCharmMiscProject/boletin3.py
@@ -0,0 +1,36 @@
+#Ejercicio 1
+num = float (input("Dime un numero:"))
+if num > 0 :
+   print("E un numero positivo")
+
+#Ejercicio 2
+numero1 = float(input("Dime un numero: "))
+numero2 = float(input("Dime un numero: "))
+
+
+if numero1 <= numero2:
+   print(numero1 - numero2)
+else:
+   print(numero1 + numero2)
+#Ejercicio 3
+
+numero1 = float(input("Digite um numero: "))
+
+if numero1 >=0:
+   print("Es positivo")
+else:
+   print("Es negativo")
+
+#Ejercicio 4
+persona1 = input("Digite o nome de una persona: ")
+persona2 = input("Digite o nome de una persona: ")
+numero1 = float(input("Digite um numero: "))
+numero2: float = float(input("Digite outro numero: "))
+
+
+if numero1 > numero2:
+   print(persona1, numero1,"A diferencia de peso eh:", numero2 - numero1)
+elif numero1 < numero2:
+   print(persona2, numero2,"A diferencia de peso eh:", numero2 - numero1)
+
+#Ejercicio 5
