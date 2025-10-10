9851d48eb1bf3425dfd04b8d2856e64936a674f9
josue <josuerodriguezbuarin@danielcastelao.org>
2025-10-10 10:23:04 +0200

    Boletin 1

---
+++ PyCharmMiscProject/BOLETIN1.py
@@ -0,0 +1,67 @@
+#1
+resultadoA = ((3 + 2) % 2 - 15) / 2 * 5
+print(resultadoA)
+
+resultadoB = (6 + (6) / 7) + 35 / 2 - 8 * 5 / 4 * 2
+print(resultadoB)
+
+resultadoC = 3 + 6 * 14 % 3
+print(resultadoC)
+
+resultadoD = 8 + 7 * 3 + 4 * 6 /2 % 4
+print(resultadoD)
+
+resultadoE = 27 % 4 +15 / 4
+print(resultadoE)
+
+resultadoF = 37 / 4**2 - 2
+print(resultadoF)
+
+resultadoG = 9 * 2 / 3 * 25 * 3
+print(resultadoG)
+
+resultadoH = (7 * 3 - 4 * 4) * 2 / 4 * 2
+print(resultadoH)
+#ejercicio 2
+
+#a)
+Salto- mortal=Salto_mortal
+salto_mortal= bien
+salto + mortal= No se puede poner un mas
+2salto=No puede empezar por un número
+ “salto”= No esta bien, si tiene comillas se convierte en un string, no es una variable
+
+#b)
+cantidade total= No puede tener espacio
+cant_total= bien
+cant5= bien
+cantidadeTotal= bien
+EJERCICIO 3
+#Expresar, utilizando operadores aritméticos, as seguintes expresións
+#a)(m+n)/n		b)((m+n)/p)/((p-r)/s)                  c) (m+4)/(p-q)
+
+
+#d)(c+r+t)/100                        e)(m+n)/(p+(q/r))            f)(m/n)*(p+q)
+
+
+
+#g)(n(1+i)**t * i)/((1+i)**t - 1)
+#EJERCICIO 4
+#a) true and true == false/falso, true and true es true, no cambia nada, asi que con andaría verdadera, por lo que esta afirmación es falsa
+#b) not false == true/Verdadero, not invierte el resultado, asi que si pone not con false pasará a ser el contrario, true, así que esta bien
+#c) (true and true) or false == true/Verdadero, true and true= true, or hace que si hay una true, aunque la otra sea false será true, así que esta bien
+#d) (false or false) and false != true=Verdade, false or false=false, and false seguirá siendo false, pero al poner la ! invierte el resultado, asi que será true, esta bien
+#e) (not(true and false)) == false/Falso, true and false=false, no invertirá el resultado, dará true, asi que esta mal.
+#f) “12” + “12” == “24”= Mal, con comillas + concatena, es un string, no una operación aritmética
+#g) “34” + “43” == “3443”=Esta bien por lo mismo que la anterior
+
+EJERCICIO 5
+#a) i = 1, j = 0, k = 1 				i + k <= j – k * 3 and k >= 2==FALSE
+#b) i = 3, j = 2, k = -1				i == 3 or j <= 2 and k > 0==TRUE
+#c) tipo = 10, rede = 7,5 			tipo < rede + 1,5==FALSE
+#d) ano = 1993				ano % 400 == 0==FALSE
+#e) 3 == 2 or 5 > 1 + 1 ==TRUE
+#f ) 5 – 2 > 4 and not(0,5 == 1 / 5)==FALSE
+#g) a = 2, b = 5, c = 6, d = 10==FALSE		a >= b or a >= c and a <d
+#h) a = 2, b = 5, c = 6, d = 10		a + b < c and a + c < d or 2 * a < a + b==TRUE
+#i)  a = 2, b = 5, c = 6, d = 10		!(a * b < d) and !(a * b < c) or b + c <= d==TRUE
