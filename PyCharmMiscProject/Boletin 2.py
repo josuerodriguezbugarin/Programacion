f67c96280b56296977fab250ef00947f7f476fd0
josue <josuerodriguezbuarin@danielcastelao.org>
2025-10-10 10:28:50 +0200

    Boletin 2

---
+++ PyCharmMiscProject/Boletin 2.py
@@ -0,0 +1,35 @@
+#Ejercicio 1
+Base = 4
+altura = 3
+
+
+print(Base * altura/2)
+#Ejercicio 2
+lado = 3
+
+area = lado*lado
+print(area)
+
+#Ejercicio 3
+Euro = 10
+Dolar= 1.18
+print(Euro*Dolar)
+#Ejercicio 4
+numero1 = 10
+numero2 = 5
+
+
+sumas = numero1 + numero2
+restar = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+
+
+print(sumas)
+print(restar)
+print(multiplicacion)
+print(division)
+#Ejercicio 5
+millas = 10
+metros = millas*1852
+print(metros)
