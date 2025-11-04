2543fd4947a898abb2a722dfcb0f7484fd10526d
josue <josuerodriguezbuarin@danielcastelao.org>
2025-11-04 13:30:57 +0100

    examen resuelto 1

---
+++ EXAMEN/EXamen.py
@@ -0,0 +1,20 @@
+temperatura = []
+dias = ["Lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo"]
+for i in dias:
+    decir = int(input("Que temaperaturas hay esta semana"))
+    temperatura.append(decir)
+
+for a in range(len(dias)):
+    print(f"El {dias[a]} hace {temperatura[a]}")
+
+def calculo_temp(dias):
+    for i in range(len(dias)):
+        if dias[i] > dias[i + 1]:
+            return False
+    return True
+print(calculo_temp(dias))
+def calculo_media(dias):
+    if dias = 0
+    
+
+def calculo_valor(dias):
